6637dd790ddae3a90865c94662abc31b7bf7c754
rds80 <rippleshah@msn.com>
2018-07-05 23:36:04 -0400

    Updated custom type example

--- C7Nutshell/Program.cs
+++ C7Nutshell/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace C7Nutshell
 {
@@ -6,10 +7,20 @@ namespace C7Nutshell
     {
         static void Main(string[] args)
         {
+            CustomTypeExample();
+
+
+        }
+
+        private static void CustomTypeExample()
+        {
             UnitConverter feetToInchesConverter = new UnitConverter(12);
             UnitConverter milesToFeetConverter = new UnitConverter(5280);
 
-
+            WriteLine(feetToInchesConverter.Convert(30));   //360
+            WriteLine(feetToInchesConverter.Convert(100));   //1200
+            WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));   //63360
+            Read();
         }
     }
 }
