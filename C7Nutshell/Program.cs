2e7744dce3e5817a50820226e5969cabb2aa1b11
rds80 <rippleshah@msn.com>
2018-07-05 23:29:10 -0400

    Examples from C# 7.0 in a Nutshell

---
+++ C7Nutshell/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C7Nutshell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitConverter feetToInchesConverter = new UnitConverter(12);
+            UnitConverter milesToFeetConverter = new UnitConverter(5280);
+
+
+        }
+    }
+}
---
+++ C7Nutshell/UnitConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C7Nutshell
+{
+    public class UnitConverter
+    {
+        int ratio;  //Field
+        public UnitConverter(int unitRatio) { ratio = unitRatio; }  //Constructor
+        public int Convert (int unit) { return unit * ratio; } //Method
+    }
+}
