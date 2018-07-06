353b3a265d45bcca7f0e87b6e0afc6a4974b83bb
rds80 <rippleshah@msn.com>
2018-07-06 00:22:51 -0400

    Added example of Instance vs Static members

---
+++ C7Nutshell/Panda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C7Nutshell
+{
+    public class Panda
+    {
+        public string name; //instance field
+        public static int population; //static field
+
+        public Panda(string n)
+        {
+            name = n; //assigng instance field
+            population += 1; //incrememnt static population field
+        }
+    }
+}
--- C7Nutshell/Program.cs
+++ C7Nutshell/Program.cs
@@ -7,9 +7,18 @@ namespace C7Nutshell
     {
         static void Main(string[] args)
         {
-            CustomTypeExample();
+            //CustomTypeExample();
+            InstanceStaticExample();
+            Read();
+        }
 
-
+        private static void InstanceStaticExample()
+        {
+            Panda p1 = new Panda("Pan Dee");
+            Panda p2 = new Panda("Pan Dah");
+            WriteLine(p1.name);
+            WriteLine(p2.name);
+            Write(Panda.population);
         }
 
         private static void CustomTypeExample()
@@ -20,7 +29,6 @@ namespace C7Nutshell
             WriteLine(feetToInchesConverter.Convert(30));   //360
             WriteLine(feetToInchesConverter.Convert(100));   //1200
             WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));   //63360
-            Read();
         }
     }
 }
