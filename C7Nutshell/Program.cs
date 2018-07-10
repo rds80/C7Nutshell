58c6c6fed360631b77f9e4119ba39515eab4acb6
rds80 <rippleshah@msn.com>
2018-07-09 20:41:36 -0400

    Added Stack and Heap examples

--- C7Nutshell/Program.cs
+++ C7Nutshell/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using static System.Console;
 
 namespace C7Nutshell
@@ -10,11 +11,26 @@ namespace C7Nutshell
             //CustomTypeExample();
             //InstanceStaticExample();
             //StructExample();
-            ClassExample();
+            //ClassExample();
+            //NumericalLiteralExamples();
+            //FactorialIterationExample(6);
+            //int factorial = FactorialRecursionExample(6);
+            //CheckedOperatorExample();
+            //DivideNonZeroExample();
+            //NaNExample();
+            //BoolReferenceExample();
+            //Point[] a = new Point[1000];
+            //int x = a[500].X;
+            //HeapGCExample();
+            DefiniteAssignmentExample();
             Read();
         }
 
-        public struct Point { public int X, Y; }
+        public struct Point
+        {
+            public int X; //4 bytes
+            public int Y; //4 bytes
+        }
 
         private static void StructExample()
         {
@@ -76,5 +92,103 @@ namespace C7Nutshell
             WriteLine(feetToInchesConverter.Convert(100));   //1200
             WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));   //63360
         }
+
+        private static void NumericalLiteralExamples()
+        {
+            WriteLine(1.0.GetType());   //Double (double)
+            WriteLine(1E06.GetType());   //Double (double)
+            WriteLine(1.GetType());   //Int32 (int)
+            WriteLine(0xF0000000.GetType());   //uint32 (uint)
+            WriteLine(0x100000000.GetType());   //int64 (long)
+        }
+
+        private static int FactorialIterationExample(int number)
+        {
+            int factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+
+        private static int FactorialRecursionExample(int number)
+        {
+
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * FactorialRecursionExample(number - 1);
+
+        }
+
+        private static void CheckedOperatorExample()
+        {
+            int a = 1000000;
+            int b = 1000000;
+
+            int c = checked(a * b);
+        }
+
+        private static void DivideNonZeroExample()
+        {
+            WriteLine(1.0 / 0.0);   //Infinity
+            WriteLine(-1.0 / 0.0);   // -Infinity
+            WriteLine(1.0 / -0.0);   // -Infinity
+            WriteLine(-1.0 / -0.0);   // -Infinity
+        }
+
+        private static void NaNExample()
+        {
+            WriteLine(0.0 / 0.0);   //NaN
+            WriteLine((1.0 / 0.0) - (1.0 / 0.0)); //NaN
+        }
+
+        private static void BoolReferenceExample()
+        {
+            Dude d1 = new Dude("John");
+            Dude d2 = new Dude("John");
+            WriteLine(d1 == d2); //False
+            Dude d3 = d1;
+            WriteLine(d1 == d3);  //True
+        }
+
+        public class Dude
+        {
+            public string Name;
+            public Dude(string n)
+            {
+                Name = n;
+            }
+        }
+
+        private static void DefiniteAssignmentExample()
+        {
+            int[] ints = new int[2];
+            WriteLine(ints[0]);
+        }
+
+        private static void HeapGCExample()
+        {
+            StringBuilder ref1 = new StringBuilder("object1");
+            WriteLine(ref1);
+
+            //SB referenced by ref is now eligible for GC.
+
+            StringBuilder ref2 = new StringBuilder("object2");
+            StringBuilder ref3 = ref2;
+
+            //StringBuilder referenced by ref2 is not yet eligible for GC.
+            WriteLine(ref3);    //object2
+        }
+
+        public class Test
+        {
+            public static int x;
+        }
+
     }
 }
